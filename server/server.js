c7641a54d348b07b76d8e3a4f41a7038072549c3
Mikiyas Alemayehu <mikiyasalemayehu537@gmail.com>
2025-05-17 10:58:49 +0300

    the cpmlited backend

--- server/server.js
+++ server/server.js
@@ -3,11 +3,11 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connect } from "http2";
-//import { Server } from "socket.io";
+import { Server } from "socket.io";
 import { connectDB } from "./lib/db.js";
-// import messageRouter from "./routes/messageRoutes.js";
+ import messageRouter from "./routes/messageRoutes.js";
 // import userRoutes from './routes/userRoutes.js';
-//  import userRouter from "./routes/userRoutes.js";
+ import userRouter from "./routes/userRoutes.js";
 
 
 
@@ -15,52 +15,49 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const server = http.createServer(app) // Renamed to avoid conflict
 
-/* Initialize socket.io
-const io = new Server(httpServer, {
+// Initialize socket.io
+ export const io = new Server(server, {
     cors: { origin: "*" }
-});
-*/
+})
 
 
-/* Store online users
-export const userSocketMap = {};
- */
 
-/* Socket.io connection handler
+// Store online users
+export const userSocketMap = {}; // Store userId and socketId mapping
+ 
+
+// Socket.io connection handler
 io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User connected", userId);
     
     if (userId) {
         userSocketMap[userId] = socket.id;
-    }
+    
 
     
 // emit online user to all connected clients
-    io.emit("getOnline-users", Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
         console.log("User disconnected", userId);
         delete userSocketMap[userId];
         io.emit("getOnlineUsers", Object.keys(userSocketMap))
     })
-});
-export { io };
-*/
+}});
+
+   //        export { io };
+
 
 // Middleware
 app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 // Routes setup
-app.use("/api/status", (req, res) => {
-    res.send("Server is live");
-});
-
-/*
+app.use("/api/status", (req, res) =>  res.send("Server is live"));
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
-*/
+
 
 
 // Connect to MongoDB and start server
